ea5f03392ccac5b37e948999602471a5993d432e
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-12-11 08:21:06 +0800

    event migration

---
+++ db/migrate/20251211002016_create_events.rb
@@ -0,0 +1,10 @@
+class CreateEvents < ActiveRecord::Migration[8.0]
+  def change
+    create_table :events do |t|
+      t.text :description
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
