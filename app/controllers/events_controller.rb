cc52d9b8196ecde5dc4d8ca41cc651e7f3ef54d0
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-12-12 09:16:05 +0800

    save new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,28 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @event=Event.new
+  end
+
   def index
+  end
+
+  def create
+    @event=current_user.events.build(event_params)
+    puts "hiii"
+
+    if @event.save
+      redirect_to users_show_path
+      # redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:description)
   end
 end
